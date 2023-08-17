e78e3b1b53bdfdf580d4f230e4adefd21c03b35a
developer-nieem <naimhossenwork@gmail.com>
2023-08-17 22:54:01 +0600

    setup projects and create header html

---
+++ main.js
@@ -0,0 +1,10 @@
+import './assets/css/style.min.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
+
+document.querySelector('#app').innerHTML = `
+  
+`
+
+setupCounter(document.querySelector('#counter'))
