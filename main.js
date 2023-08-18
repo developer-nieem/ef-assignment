25627bcd4b614ccb6b3be870c04ee53ce71497a5
developer-nieem <naimhossenwork@gmail.com>
2023-08-18 22:48:54 +0600

    mega menu added

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-import './assets/css/style.min.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+
+// mega menu
+const megaMenuItems = document.querySelectorAll('.has-mega');
+
+console.log(megaMenuItems);
+
+megaMenuItems.forEach( item=> {
+    const link = item.querySelector('a');
+    console.log(link);
+    const megaMenu = item.querySelector('.mega-menu');
+    link.addEventListener('click', (event) => {
+        console.log('clicked');
+        event.preventDefault(); 
+        item.classList.toggle('active')
+        megaMenu?.classList.toggle('show');
+      });
+})
 
 
-document.querySelector('#app').innerHTML = `
-  
-`
-
-setupCounter(document.querySelector('#counter'))
